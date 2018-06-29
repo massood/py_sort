4b58460cf9f50029c52fb0e2262667a80fa2725a
massood <massood@email.com>
2018-06-29 17:44:42 +0430

    Code cleanup; use `range` instead of `while`

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 l = [7, 5, 6, 1, 2, 4, 3]
 
 def sort(arr):
-	n = len(arr)
-	while n > 1:
+	for n in range(len(arr), 1, -1):
 		for i in range(1, n):
 			if (arr[i-1] > arr[i]):
 				arr[i-1], arr[i] = arr[i], arr[i-1]
-		n -= 1
 
 
 print ("before sorting: ")
